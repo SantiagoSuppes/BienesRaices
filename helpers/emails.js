b3248b6e677bb9a3f62d241fff49e077f655a65b
Santiago Suppes <slsuppes@gmail.com>
2025-05-15 17:16:30 -0300

    nodemailer added

---
+++ helpers/emails.js
@@ -0,0 +1,16 @@
+import nodemailer from 'nodemailer'
+
+const emailRegistro = aync (datos) => {
+    var transport = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+}
+
+export {
+    emailRegistro
+}
